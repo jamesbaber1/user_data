750b2d9c789340db76d2761f07ed520cd1477426
jamesbaber1 <31859220+jamesbaber1@users.noreply.github.com>
2020-09-13 16:21:09 -0500

    incomplete: saving some work with merging dataframes

--- strategies/BB_Strategy03.py
+++ strategies/BB_Strategy03.py
@@ -4,13 +4,16 @@
 import numpy as np  # noqa
 import pandas as pd  # noqa
 from pandas import DataFrame
-
+from datetime import datetime
 from freqtrade.strategy.interface import IStrategy
 
 # --------------------------------
 # Add your lib to import here
 import talib.abstract as ta
 import freqtrade.vendor.qtpylib.indicators as qtpylib
+import logging
+logger = logging.getLogger('freqtrade.worker')
+import os
 
 
 class BB_Strategy03(IStrategy):
@@ -53,10 +56,13 @@ class BB_Strategy03(IStrategy):
     # trailing_stop_positive_offset = 0.0  # Disabled / not configured
 
     # Optimal ticker interval for the strategy.
-    ticker_interval = '1d'
+    ticker_interval = '1m'
 
     # Run "populate_indicators()" only for new candle.
     process_only_new_candles = False
+
+    # disable dataframe from being checked so we can modify it and it is not invalidated
+    disable_dataframe_checks = True
 
     # These values can be overridden in the "ask_strategy" section in the config.
     use_sell_signal = True
@@ -110,13 +116,27 @@ class BB_Strategy03(IStrategy):
         :param metadata: Additional information, like the currently traded pair
         :return: a Dataframe with all mandatory indicators for the strategies
         """
+        informative_time_frame = '1d'
+        informative = None
+
         if not self.dp:
             # Don't do anything if DataProvider is not available.
             return dataframe
 
-        inf_tf = '1d'
-        # Get the informative pair
-        informative = self.dp.get_pair_dataframe(pair=metadata['pair'], timeframe=inf_tf)
+        if self.dp:
+            if self.dp.runmode.value in ('live', 'dry_run'):
+                now = datetime.utcnow()
+                time = pd.Timestamp(year=now.year, month=now.month, day=now.day, tz="GMT+0")
+
+                ticker = self.dp.ticker(metadata['pair'])
+                new_row = {'date': time, 'open': 1, 'high': 1, 'low': 1, 'close': ticker['last'], 'volume': 1}
+
+                # Get the informative pair
+                informative = self.dp.get_pair_dataframe(pair=metadata['pair'], timeframe=informative_time_frame)
+                informative = informative.append(new_row, ignore_index=True)
+
+        # if not informative:
+        #     return dataframe
 
         # calculate the bollinger bands with 1d candles
         bollinger = qtpylib.bollinger_bands(informative['close'], window=3, stds=1)
@@ -125,16 +145,42 @@ class BB_Strategy03(IStrategy):
         informative[f'bb_upperband1'] = bollinger['upper']
 
         # Rename columns to be unique
-        informative.columns = [f"{col}_{inf_tf}" for col in informative.columns]
         # Assuming inf_tf = '1d' - then the columns will now be:
-        # date_1d, open_1d, high_1d, low_1d, close_1d, rsi_1d
+        # date_1d, open_1d, high_1d, low_1d, close_1d
+        informative.columns = [f"{col}_{informative_time_frame}" for col in informative.columns]
+
+        # sync up dates
+        # informative[f'date_{informative_time_frame}'] = pd.to_datetime(informative[f'date_{informative_time_frame}'], utc=True)
+        # dataframe['date'] = pd.to_datetime(dataframe['date'], utc=True)
+
+        pd.set_option('display.max_columns', None)
+        pd.set_option('display.width', 300)
+        logger.info(f'---------Informative Pair: {metadata["pair"]}-------------------')
+        path = os.path.normpath(os.path.abspath(os.path.join(os.getcwd(), 'user_data', f"dataframe_{metadata['pair'].replace('/', '')}.csv")))
+        logger.info(path)
+        file = open(path, "w")
+        file.write(dataframe.to_csv())
+        file.close()
+
+        logger.info(f'\n\n{informative.to_markdown()}')
 
         # Combine the 2 dataframes
         # all indicators on the informative sample MUST be calculated before this point
-        dataframe = pd.merge(dataframe, informative, left_on='date', right_on=f'date_{inf_tf}', how='left')
+        dataframe = dataframe.merge(
+            informative,
+            left_on='date',
+            right_on=f'date_{informative_time_frame}',
+            how='left'
+        )
+
         # FFill to have the 1d value available in every row throughout the day.
         # Without this, comparisons would only work once per day.
-        dataframe = dataframe.ffill()
+        # dataframe = dataframe.ffill()
+
+        pd.set_option('display.max_columns', None)
+        pd.set_option('display.width', 300)
+        logger.info(f'---------Dataframe Pair: {metadata["pair"]}-------------------')
+        logger.info(f'\n\n{dataframe.to_markdown()}')
 
         return dataframe
 
